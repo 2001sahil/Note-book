a8b76d774da48a0912b74a918c2e55c76051ec8b
2001sahil <114495568+2001sahil@users.noreply.github.com>
2023-04-02 17:20:50 +0530

    checking
    trying

--- App.js
+++ App.js
@@ -10,10 +10,10 @@ const port=8000
 app.use(express.json())
 app.use(cors())
 // static files////////////////////
-app.use(express.static(path.join(__dirname,'./my-app/build')))
+app.use(express.static(path.join(__dirname,'../my-app/build')))
 
-app.get("*", (req,res)=>{
-    res.sendFile(path.join(__dirname,'./my-app/build/index.html'))
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname,'../my-app/build/index.html'))
 })
 
 
